d1887c2a18d5101d64ce4dea4cd12271589f0433
MarrMika <slobodjanmarija1@gmail.com>
2020-10-07 20:47:59 +0300

    Init application

---
+++ src/test/java/com/dimsirka/animalservice/AnimalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimsirka.animalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
