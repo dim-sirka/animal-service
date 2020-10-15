d1ec0c336f164f2a747f26b658ef621dd5363264
MarrMika <slobodjanmarija1@gmail.com>
2020-10-15 10:34:52 +0300

    Fix animal table name

--- src/main/java/com/dimsirka/animalservice/entities/Animal.java
+++ src/main/java/com/dimsirka/animalservice/entities/Animal.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "admins")
+@Table(name = "animals")
 public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
